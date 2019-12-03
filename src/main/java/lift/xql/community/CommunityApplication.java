cd9c113b8d34a665ece577281f0465cc9e85766c
xql269949008 <269949008@qq.com>
2019-12-03 20:17:37 +0800

    init repo

---
+++ src/main/java/lift/xql/community/CommunityApplication.java
@@ -0,0 +1,10 @@
+package lift.xql.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityApplication {
+    public static void main(String[] args){
+        SpringApplication.run(CommunityApplication.class, args);}
+}
